1e041a99849a9271e03f981b53306b6c175a108c
Taras Pobyvanets <furentes.t@gmail.com>
2020-03-26 23:19:54 +0200

    Homework has done

--- Program.cs
+++ Program.cs
@@ -12,8 +12,16 @@ namespace HomeworkTemplate
                 // Your solution goes here
                 // You can get all needed inputs from task.[Property]
                 // Good luck!
+                System.Globalization.NumberFormatInfo info = new System.Globalization.NumberFormatInfo();
 
-                return "My answer";
+                info.CurrencySymbol = "$";
+
+                string symbols = new string(task.DecorativeSign, Task2.Padding / 2);
+                string spaces = new string(' ', Task2.Padding / 2);
+                //string const 
+                string answer = string.Format("{0}{1}{2}{1}{0}", symbols, spaces, task.Balance.ToString("c2", info));
+
+                return answer;
             };
 
             Task2.CheckSolver(TaskSolver);
